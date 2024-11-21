3b3b7e5cc4ef0f6d16b19015a32922d768d987a9
kihwn <cristine980@konkuk.ac.kr>
2024-11-21 14:56:33 +0900

    :sparkles: Feat:Display different images based on event sport
    넘겨받은 대회 데이터로부터 대회 이미지가 null값일 경우 대회의 종목에 맞는 이미지 표시 로직 구현

--- calog-project/src/components/EventModal.vue
+++ calog-project/src/components/EventModal.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="modal-overlay" @click.self="close">
       <div class="modal-content">
-        <button class="close-button" @click="close">X</button>
+        <button class="close-button" @click="close">X</button> <!-- 모달창 끄는 버튼 --> 
         <h2>{{ event.eventName }}</h2>
         <p><strong>종목:</strong> {{ event.sport }}</p>
         <p><strong>일시:</strong> {{ formatDate(event.eventDate) }}</p>
         <p><strong>장소:</strong> {{ event.address }}, {{ event.city }}</p>
         <p><strong>등록 시작:</strong> {{ formatDate(event.registrationStart) }}</p>
         <p><strong>등록 마감:</strong> {{ formatDate(event.registrationDeadline) }}</p>
-        <img v-if="event.img" :src="event.img" alt="Event Image" />
-      </div>
+        <img :src="getEventImage(event)" alt="Event Image" />
+        </div>
     </div>
   </template>
   
@@ -30,6 +30,29 @@
         const options = { year: "numeric", month: "long", day: "numeric" };
         return new Date(date).toLocaleDateString("ko-KR", options);
       },
+      // 대회의 이미지가 존재하지 않을 경우 대회의 종목에 따라 대표 이미지 변경하는 메서드
+      getEventImage(event) {
+        if (!event.img) {
+          if (event.sport === "마라톤") {
+            return new URL('@/assets/sport-image/marathon.jpg', import.meta.url).href;
+          } else if (event.sport === "그란폰도") {
+            return new URL('@/assets/sport-image/granfondo.jpg', import.meta.url).href;
+          } else if (event.sport === "테니스") {
+            return new URL('@/assets/sport-image/tennis.jpg', import.meta.url).href;
+          } else if (event.sport ==="배드민턴") {
+            return new URL('@/assets/sport-image/badminton.jpg', import.meta.url).href;
+          } else if (event.sport ==="철인3종") {
+            return new URL('@/assets/sport-image/triathlon.jpg', import.meta.url).href;
+          } 
+        
+        else {
+          return new URL('@/assets/sport-image/default.jpg', import.meta.url).href;
+          //혹시 몰라 기본 이미지 생성
+        }
+      }
+      return event.img; 
+    },
+      
     },
   };
   </script>
@@ -57,6 +80,12 @@
     position: relative;
     box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3); 
   }
+  .modal-content img {
+  max-width: 100%;
+  height: auto;
+  border-radius: 8px;
+  margin-top: 20px;
+}
   
   .close-button {
     position: absolute;
